a17e80053cffaaba07c40faacb70969b86bc541e
Blerta Toska <36380101+Toska30@users.noreply.github.com>
2022-05-12 12:13:11 +0200

    Update index.js

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -4,13 +4,7 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
 const firebaseConfig = {
-  // apiKey: "AIzaSyDmrQD2-J8Tul6GfivSWB2nV-Jc1lm76UA",
-  // authDomain: "toy-shop-4c0fa.firebaseapp.com",
-  // projectId: "toy-shop-4c0fa",
-  // storageBucket: "toy-shop-4c0fa.appspot.com",
-  // messagingSenderId: "556783445774",
-  // appId: "1:556783445774:web:31535ed3e3d84d2d64995f",
-  // measurementId: "G-M61ZMC84BW",
+  
 
   apiKey: "AIzaSyAvvrJ0ffei75TPIMsZ64sa4nExMi1yFSM",
   authDomain: "toy-exchange-364c6.firebaseapp.com",
